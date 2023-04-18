92b599f0d2e05b07b576c4f434e06783a9baba47
Aman <singhaman021202@gmail.com>
2023-04-18 08:07:12 +0530

    commit

---
+++ app/src/main/java/com/example/gha_master/Plant_Model/Plant_Model_class.kt
@@ -0,0 +1,12 @@
+package com.example.gha_master.Plant_Model
+
+class Plant_Model_class (
+
+    val id : Int,
+    val Name : String,
+    val Humidity : Int,
+    val temperature :Double,
+    val Soil_Moisture:Double,
+    val Air_Quality:String,
+    val disease: String
+)
---
+++ app/src/main/java/com/example/gha_master/plant_Adaptor.kt
@@ -0,0 +1,31 @@
+package com.example.gha_master
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.RemoteViews.RemoteCollectionItems
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.gha_master.Plant_Model.Plant_Model_class
+
+abstract class plant_Adaptor(val context: Context, val items: ArrayList<Plant_Model_class>) :
+//RecyclerView.Adapter<plant_Adaptor.ViewHolder>() {
+
+  //  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+    //    return ViewHolder(
+      //      LayoutInflater.from(context).inflate(
+        //        R.layout.item_plant_user,
+          //      parent,
+            //    false
+           // )
+       // )
+
+   // }
+
+  // override fun onBindViewHolder(holder: ViewHolder, position:Int) {
+    //   val item = items.get(position)
+
+
+    //}
+  //}
